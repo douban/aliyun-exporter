7d2ec63430cf45bcabb3b01dcc06cc194be5b056
QSummerY <1272750872@qq.com>
2022-09-06 10:44:34 +0800

    add more status label

--- collector/acs_cdn.go
+++ collector/acs_cdn.go
@@ -17,19 +17,24 @@ func NewCdnExporter(c *cms.Client) *CdnExporter {
 	}
 }
 
-func (db *CdnExporter) RetrievehitRate() []datapoint {
+type statusPoint struct {
+	datapoint
+	Status  string  `json:"status"`
+}
+
+func (db *CdnExporter) RetrieveHitRate() []datapoint {
 	return retrieve("hitRate", db.project)
 }
 
-func (db *CdnExporter) RetrieveoriBps() []datapoint {
+func (db *CdnExporter) RetrieveOriBps() []datapoint {
 	return retrieve("ori_bps", db.project)
 }
 
-func (db *CdnExporter) Retrievel1Acc() []datapoint {
+func (db *CdnExporter) RetrieveL1Acc() []datapoint {
 	return retrieve("l1_acc", db.project)
 }
 
-func (db *CdnExporter) RetrieveoriAcc() []datapoint {
+func (db *CdnExporter) RetrieveOriAcc() []datapoint {
 	return retrieve("ori_acc", db.project)
 }
 
@@ -37,10 +42,44 @@ func (db *CdnExporter) RetrieveBPS() []datapoint {
 	return retrieve("BPS", db.project)
 }
 
-func (db *CdnExporter) Retrieveoricode4xxRatio() []datapoint {
-	return retrieve("ori_code_ratio_4xx", db.project)
+func (db *CdnExporter) RetrieveOriStatusRatio() []statusPoint {
+	statusMetrics := map[string]string{
+		"code1xx": "1xx",
+		"code2xx": "2xx",
+		"code3xx": "3xx",
+		"code4xx": "4xx",
+		"code_ratio_499": "499",
+		"code5xx": "5xx",
+	}
+	var response []statusPoint
+	for metric, status := range statusMetrics {
+		dataPoints := retrieve(metric, db.project)
+		for _, point := range dataPoints{
+			// code1xx、code2xx、code3xx 只有 Maximum 值
+			if metric == "code1xx" || metric == "code2xx" || metric == "code3xx" {
+				point.Average = point.Maximum
+			}
+			response = append(response, statusPoint{point, status})
+		}
+	}
+	return response
 }
 
-func (db *CdnExporter) Retrieveoricode5xxRatio() []datapoint {
-	return retrieve("ori_code_ratio_5xx", db.project)
+func (db *CdnExporter) RetrieveStatusRatio() []statusPoint {
+	backSourceStatusMetrics := map[string]string{
+		"ori_code_ratio_1xx": "1xx",
+		"ori_code_ratio_2xx": "2xx",
+		"ori_code_ratio_3xx": "3xx",
+		"ori_code_ratio_499": "499",
+		"ori_code_ratio_4xx": "4xx",
+		"ori_code_ratio_5xx": "5xx",
+	}
+	var response []statusPoint
+	for metric, status := range backSourceStatusMetrics {
+		dataPoints := retrieve(metric, db.project)
+		for _, point := range dataPoints{
+			response = append(response, statusPoint{point, status})
+		}
+	}
+	return response
 }
